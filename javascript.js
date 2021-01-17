086ee45d439eb6249b377ed005d78647aa98e876
muzytoto <77212838+muzytoto@users.noreply.github.com>
2021-01-17 17:24:55 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,3 +2,16 @@ var myCarousel = document.querySelector('carousel-inner')
 var carousel = new bootstrap.Carousel(carousel-inner, {
   interval: 5,
 })
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
